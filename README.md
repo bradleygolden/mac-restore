816a0fc2a4cb915337b01ca4de9ea0b4a7d6f611
Bradley Golden <golden.bradley@gmail.com>
2016-01-12 19:33:37 -0600

    updated

---
+++ README.md
@@ -0,0 +1,3 @@
+## Resources
+* Automate your mac with Launch Control - http://www.soma-zone.com/LaunchControl/
+* Fresh OSX install setup for Mac - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
---
+++ mac_restore.sh
@@ -0,0 +1,101 @@
+#!bin/bash
+
+# Check for Homebrew,
+# Install if we don't have it
+if test ! $(which brew); then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install Bash 4
+brew install bash
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+
+# Install fonts
+brew tap caskroom/fonts
+
+# Set path to use new binaries
+echo export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH" >> ~/.bash_profile
+
+brew cleanup
+
+# Binaries
+binaries=(
+  python
+  git
+  vim
+  macvim --overide-system-vim
+  tmux
+  mackup
+  rcm
+  zsh
+)
+
+echo "installing binaries..."
+brew install ${binaries[@]}
+
+brew cleanup
+
+# Apps
+apps=(
+  google-drive
+  google-chrome
+  flash
+  silverlight
+  vlc
+  dash
+  slack
+  spotify
+  iterm2
+  atom
+  microsoft-office
+  microsoft-lync
+  amazon-music
+  flux
+  kindle
+  dash
+  java
+  android-studio
+)
+
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+echo "installing apps..."
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# Configure zsh
+sudo mv /etc/zshenv /etc/zprofile
+cat /etc/shells | grep zsh || which zsh | sudo tee -a /etc/shells
+chsh -s $(which zsh)
+
+# Use keychain for storing passwords
+git config --global credential.helper osxkeychain
+
+# You might not see colors without this
+git config --global color.ui true
+
+# Create development environment
+mkdir ~/Development
+
+# Restore previous configurations using mackup
+# Make sure your storage service is installed first
+# If it's not, this will fail and will need to be ran later
+mackup restore
+
+# Clone all public repos into the development environment
+username=bradleygolden
+echo "cloning repos..."
+curl -o ~/Development —u $username -s https://api.github.com/users/$username/repos?per_page=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone #{repo["ssh_url"]} ]}'
+
