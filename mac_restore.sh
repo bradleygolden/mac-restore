9df6dcdc9bb2c47ce42ece8b174083d598310899
Bradley Golden <bgolden@cccis.com>
2017-05-16 17:40:32 -0500

    add new setup

--- mac_restore.sh
+++ mac_restore.sh
@@ -3,84 +3,27 @@
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
 fi
 
-# Update homebrew recipes
-brew update
-
-# Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-
-# Install more recent version of Bash
-brew install bash
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-
-# Install fonts
-brew tap caskroom/fonts
-
-# Set path to use new binaries
-echo export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH" >> ~/.bash_profile
-
+# install brew binaries/cask applications/app store applications
+brew bundle --file=homebrew/Brewfile
 brew cleanup
 
-# Binaries
-binaries=(
-  python
-  python3
-  git
-  vim
-  zsh
-  node
-  mackup
-)
+# setup pyenv
+echo 'eval "$(pyenv init -)"' >> ~/.zshenv
+echo "installing python versions..."
+pyenv install 2.7.13
+pyenv install 3.6.0
+pyenv global system 2.7.13 3.6.0  # set python version defaults
 
-echo "installing binaries..."
-brew install ${binaries[@]}
+# install necessary system wide pip packages
+python3 -m pip install -r pip/requirements.txt
 
-brew cleanup
-
-# install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Apps
-apps=(
-  google-drive
-  google-chrome
-  firefox
-  flash
-  silverlight
-  vlc
-  iterm2
-  atom
-  flux
-  dropbox
-)
-
-# Install apps to /Applications
-# Default is: /Users/$user/Applications
-echo "installing apps..."
-brew cask install --appdir="/Applications" ${apps[@]}
-
-# Configure zsh
-sudo mv /etc/zshenv /etc/zprofile
-cat /etc/shells | grep zsh || which zsh | sudo tee -a /etc/shells
-chsh -s $(which zsh)
-
-# Use keychain for storing passwords
-git config --global credential.helper osxkeychain
-
-# You might not see colors without this
-git config --global color.ui true
+# setup atom
+echo "installing atom plugins..."
+apm install --packages-file atom/package-list.txt
 
 # Create development folder
 mkdir ~/Development
-
-# After installation, set up google drive or dropbox and use 'mackup restore'
